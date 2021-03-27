79f86d87c606c9abe3e24e515b38be0e15dd4bd1
Jeremias Saldanha <57194226+thejeremias@users.noreply.github.com>
2021-03-26 22:25:03 -0300

    Delete src directory

--- src/main/java/br/edu/ifrn/comics/repository/ArquivoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifrn.comics.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifrn.comics.dominio.Arquivo;
-
-/** A classe ArquivoRepository representa o repositório do arquivo.*/
-public interface ArquivoRepository extends JpaRepository<Arquivo, Long>{
-}
